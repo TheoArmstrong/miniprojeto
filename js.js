c5bb9b9b2d056ea05fdbbda4b13ca156d425ffc7
Theo Armstrong Pedroso <theo10ap@gmail.com>
2025-11-05 14:18:31 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,78 @@
+
+var nota, LUrgente=[], LNUrgente=[], i, Nnota, index;
+
+// função para adicionar nota
+function addNota() {
+    // pegar texto da nota
+    nota = document.getElementById("Nota").value
+
+    // pegar radio selecionado
+    var radioSelecionado = document.querySelector("input[name='urgencia']:checked");
+    if(radioSelecionado.value === "NaoUrgente"){
+        LNUrgente.push(nota)
+        document.getElementById('listaNãoUrgente').innerHTML = '';
+        for(i=0;i<LNUrgente.length; i++){
+            document.getElementById('listaNãoUrgente').innerHTML += `<li>${LNUrgente[i]}</li>`;
+        }
+    } 
+
+    if(radioSelecionado.value === "Urgente"){
+        LUrgente.push(nota)
+        document.getElementById('listaUrgente').innerHTML = '';
+        for(i=0;i<LUrgente.length; i++){
+            document.getElementById('listaUrgente').innerHTML += `<li>${LUrgente[i]}</li>`;
+        }
+    }
+    
+    // limpar input e desmarcar radio
+    document.getElementById('Nota').value = '';
+    radioSelecionado.checked = false;
+
+    alert("Nota adicionada com sucesso!");
+}
+
+// função para excluir notas
+function excluirNota() {
+    // pegar numero da nota
+    Nnota = parseInt(document.getElementById("NotaExcluir").value)
+
+    var radioSelecionado = document.querySelector("input[name='urgenciaExcluir']:checked");
+    if(!radioSelecionado) {
+        alert("Por favor, selecione o tipo de nota a excluir (Urgente ou Não Urgente)");
+        return;
+    }
+
+    if(radioSelecionado.value === "NaoUrgente"){
+        if(Nnota < 1 || Nnota > LNUrgente.length) {
+            alert("Número de nota inválido! Por favor, insira um número entre 1 e " + LNUrgente.length);
+            return;
+        }
+        index = Nnota - 1;
+        LNUrgente.splice(index, 1);
+        document.getElementById('listaNãoUrgente').innerHTML = '';
+        for(i=0; i<LNUrgente.length; i++){
+            document.getElementById('listaNãoUrgente').innerHTML += `<li>${LNUrgente[i]}</li>`;
+        }
+    }
+    if(radioSelecionado.value === "Urgente"){
+        if(Nnota < 1 || Nnota > LUrgente.length) {
+            alert("Número de nota inválido! Por favor, insira um número entre 1 e " + LUrgente.length);
+            return;
+        }
+        index = Nnota - 1;
+        LUrgente.splice(index, 1);
+        document.getElementById('listaUrgente').innerHTML = '';
+        for(i=0; i<LUrgente.length; i++){
+            document.getElementById('listaUrgente').innerHTML += `<li>${LUrgente[i]}</li>`;
+        }
+    }
+    alert("Nota excluída com sucesso!");
+}
+
+function excluirNotas(){
+    LUrgente = []
+    LNUrgente = []
+    document.getElementById('listaUrgente').innerHTML = '';
+    document.getElementById('listaNãoUrgente').innerHTML = '';
+    alert("Todas as notas foram excluídas com sucesso!");
+};
