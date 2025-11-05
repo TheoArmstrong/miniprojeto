e762807351b2c324b2f5384fdff2ad74a08974a0
Theo Armstrong Pedroso <theo10ap@gmail.com>
2025-11-05 14:55:38 -0300

    aaa

--- js.js
+++ js.js
@@ -1,29 +1,38 @@
 
-var nota, LUrgente=[], LNUrgente=[], i, Nnota, index;
+var nota, LUrgente = [], LNUrgente = [], Nnota, index;
+var colorClasses = ['azul', 'verde', 'roxo'];
+
+// coloca a lista na tela
+function renderList(listArray, containerId) {
+    var container = document.getElementById(containerId);
+    // limpa container
+    container.innerHTML = '';
+    for (var j = 0; j < listArray.length; j++) {
+        var li = document.createElement('li');
+        var inner = document.createElement('div'); 
+        inner.className = colorClasses[j % colorClasses.length];
+        inner.textContent = listArray[j];
+        li.appendChild(inner);
+        container.appendChild(li);
+    }
+}
 
 // função para adicionar nota
 function addNota() {
     // pegar texto da nota
-    nota = document.getElementById("Nota").value
+    nota = document.getElementById("Nota").value.trim();
 
     // pegar radio selecionado
     var radioSelecionado = document.querySelector("input[name='urgencia']:checked");
-    if(radioSelecionado.value === "NaoUrgente"){
-        LNUrgente.push(nota)
-        document.getElementById('listaNãoUrgente').innerHTML = '';
-        for(i=0;i<LNUrgente.length; i++){
-            document.getElementById('listaNãoUrgente').innerHTML += `<li>${LNUrgente[i]}</li>`;
-        }
-    } 
 
-    if(radioSelecionado.value === "Urgente"){
-        LUrgente.push(nota)
-        document.getElementById('listaUrgente').innerHTML = '';
-        for(i=0;i<LUrgente.length; i++){
-            document.getElementById('listaUrgente').innerHTML += `<li>${LUrgente[i]}</li>`;
-        }
+    if (radioSelecionado.value === "NaoUrgente") {
+        LNUrgente.push(nota);
+        renderList(LNUrgente, 'listaNãoUrgente');
+    } else if (radioSelecionado.value === "Urgente") {
+        LUrgente.push(nota);
+        renderList(LUrgente, 'listaUrgente');
     }
-    
+
     // limpar input e desmarcar radio
     document.getElementById('Nota').value = '';
     radioSelecionado.checked = false;
@@ -34,37 +43,31 @@ function addNota() {
 // função para excluir notas
 function excluirNota() {
     // pegar numero da nota
-    Nnota = parseInt(document.getElementById("NotaExcluir").value)
+    Nnota = parseInt(document.getElementById("NotaExcluir").value);
 
     var radioSelecionado = document.querySelector("input[name='urgenciaExcluir']:checked");
-    if(!radioSelecionado) {
-        alert("Por favor, selecione o tipo de nota a excluir (Urgente ou Não Urgente)");
-        return;
-    }
 
-    if(radioSelecionado.value === "NaoUrgente"){
+    if (radioSelecionado.value === "NaoUrgente") {
         index = Nnota - 1;
         LNUrgente.splice(index, 1);
-        document.getElementById('listaNãoUrgente').innerHTML = '';
-        for(i=0; i<LNUrgente.length; i++){
-            document.getElementById('listaNãoUrgente').innerHTML += `<li>${LNUrgente[i]}</li>`;
+        renderList(LNUrgente, 'listaNãoUrgente');
+    } else if (radioSelecionado.value === "Urgente") {
+        if (Nnota < 1 || Nnota > LUrgente.length) {
+            alert("Número de nota inválido! Por favor, insira um número entre 1 e " + LUrgente.length);
+            return;
         }
-    }
-    if(radioSelecionado.value === "Urgente"){
         index = Nnota - 1;
         LUrgente.splice(index, 1);
-        document.getElementById('listaUrgente').innerHTML = '';
-        for(i=0; i<LUrgente.length; i++){
-            document.getElementById('listaUrgente').innerHTML += `<li>${LUrgente[i]}</li>`;
-        }
+        renderList(LUrgente, 'listaUrgente');
     }
     alert("Nota excluída com sucesso!");
 }
 
-function excluirNotas(){
-    LUrgente = []
-    LNUrgente = []
-    document.getElementById('listaUrgente').innerHTML = '';
-    document.getElementById('listaNãoUrgente').innerHTML = '';
+// função para excluir todas as notas
+function excluirNotas() {
+    LUrgente = [];
+    LNUrgente = [];
+    renderList(LUrgente, 'listaUrgente');
+    renderList(LNUrgente, 'listaNãoUrgente');
     alert("Todas as notas foram excluídas com sucesso!");
 };
